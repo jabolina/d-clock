012fb34b8e40df4b6b6e6b337142cfb54ce78787
José Augusto Bolina <joseaugusto.bolina@hotmail.com>
2020-11-27 14:10:26 -0300

    Consensus internals (#2)
    Created default consensus interface and Command.

--- pkg/d_clock/clock.go
+++ pkg/d_clock/clock.go
@@ -1,14 +1,26 @@
 package d_clock
 
+import (
+	"errors"
+)
+
+var (
+	ErrClockDestroyed = errors.New("clock is already destroyed")
+)
+
 // The generic interface a Clock must have. Using
 // this interface will have basic functionalities.
 type Clock interface {
 	// The clock increases by 1.
-	Tick() (uint64, error)
+	Tick() error
 
 	// Returns the current clock value.
 	Tack() (uint64, error)
 
 	// Leap the clock to the given time.
-	Leap(uint64) (uint64, error)
+	Leap(uint64) error
+
+	// Destroy the clock, after the Clock destruction every
+	// method call will panic
+	Destroy() error
 }
